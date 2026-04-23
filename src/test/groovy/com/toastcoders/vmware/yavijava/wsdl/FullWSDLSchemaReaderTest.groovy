51d7283b34b0a54586b32f09de59db9ad74a35d8
Michael Rice <michael@michaelrice.org>
2026-04-22 22:19:01 -0500

    test(wsdl): add regression test for transitive include resolution

--- src/test/groovy/com/toastcoders/vmware/yavijava/wsdl/FullWSDLSchemaReaderTest.groovy
+++ src/test/groovy/com/toastcoders/vmware/yavijava/wsdl/FullWSDLSchemaReaderTest.groovy
@@ -68,4 +68,56 @@ public class FullWSDLSchemaReaderTest {
         // Inline schema still loads; missing include is skipped (logged at WARN)
         assert schemas.size() == 1
     }
+
+    @Test
+    void testRecursivelyResolvesNestedIncludes() {
+        File tmpDir = File.createTempDir()
+        tmpDir.deleteOnExit()
+        try {
+            // Level 2: deepest XSD with a known marker type
+            new File(tmpDir, "deep.xsd").text = '''<?xml version="1.0"?>
+<schema targetNamespace="urn:vim25"
+   xmlns="http://www.w3.org/2001/XMLSchema"
+   xmlns:vim25="urn:vim25">
+   <complexType name="DeeplyNestedType">
+      <sequence/>
+   </complexType>
+</schema>'''
+
+            // Level 1: includes deep.xsd
+            new File(tmpDir, "middle.xsd").text = '''<?xml version="1.0"?>
+<schema targetNamespace="urn:vim25"
+   xmlns="http://www.w3.org/2001/XMLSchema"
+   xmlns:vim25="urn:vim25">
+   <include schemaLocation="deep.xsd" />
+</schema>'''
+
+            // Level 0: WSDL includes middle.xsd
+            File wsdl = new File(tmpDir, "test.wsdl")
+            wsdl.text = '''<?xml version="1.0"?>
+<definitions targetNamespace="urn:vim25"
+   xmlns="http://schemas.xmlsoap.org/wsdl/"
+   xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+   xmlns:vim25="urn:vim25">
+   <types>
+      <schema targetNamespace="urn:vim25"
+         xmlns="http://www.w3.org/2001/XMLSchema"
+         xmlns:vim25="urn:vim25">
+         <include schemaLocation="middle.xsd" />
+      </schema>
+   </types>
+</definitions>'''
+
+            def schemas = reader.loadSchema(wsdl)
+            // 1 inline + middle.xsd + deep.xsd = 3
+            assert schemas.size() == 3
+            // The deeply-included type must be reachable
+            boolean found = schemas.any { schema ->
+                schema.complexType.any { it.'@name'.text() == 'DeeplyNestedType' }
+            }
+            assert found, "DeeplyNestedType from level-2 include should be reachable"
+        } finally {
+            tmpDir.deleteDir()
+        }
+    }
 }
