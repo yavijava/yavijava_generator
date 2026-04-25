d45aaa53c49ba693fcb136c766463abb1afbf256
Michael Rice <michael@michaelrice.org>
2026-04-24 20:48:55 -0500

    feat(pyvmomi): add PyvmomiProperty data carrier

---
+++ src/main/groovy/com/toastcoders/vmware/yavijava/pyvmomi/PyvmomiProperty.groovy
@@ -0,0 +1,11 @@
+package com.toastcoders.vmware.yavijava.pyvmomi
+
+class PyvmomiProperty {
+    String name
+    String type
+    boolean isArray = false
+    boolean isOptional = false
+    boolean isManagedObjectReference = false
+    String version
+    String privilegeId
+}
