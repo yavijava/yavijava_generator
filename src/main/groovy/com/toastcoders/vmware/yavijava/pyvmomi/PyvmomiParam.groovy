177ed84557f4015adb79011034d42c196adc800b
Michael Rice <michael@michaelrice.org>
2026-04-24 20:50:30 -0500

    feat(pyvmomi): add PyvmomiParam, PyvmomiMethod, PyvmomiManagedObject carriers

---
+++ src/main/groovy/com/toastcoders/vmware/yavijava/pyvmomi/PyvmomiParam.groovy
@@ -0,0 +1,9 @@
+package com.toastcoders.vmware.yavijava.pyvmomi
+
+class PyvmomiParam {
+    String name
+    String type
+    boolean isArray = false
+    boolean isOptional = false
+    boolean isManagedObjectReference = false
+}
